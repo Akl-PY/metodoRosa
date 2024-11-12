de98606708d7987d351faee34295fa9733915eaa
Fernando Ortiz <ortizfernando200513@gmail.com>
2024-11-12 18:23:39 -0300

    Para Gale

---
+++ src/js/formControl.js
@@ -0,0 +1,72 @@
+function nextForm(group, step) {
+    console.log(group)
+    let actualGroup = "group" + group;  // Obtener el ID del grupo actual
+    let actualStep = actualGroup +"-"+ step
+    let nextGroup = actualGroup
+    nextStep = actualGroup +"-"+ (step+1)
+
+    if (group ==1 || group ==2 && step ==4 || group ==3 && step==3) {
+        nextGroup = "group" + (group+1); 
+        nextStep = nextGroup +"-"+ 1
+    }
+
+    console.log(actualGroup); 
+    console.log(actualStep);  
+    console.log("--------")
+    console.log(nextGroup); 
+    console.log(nextStep); 
+
+    
+    document.getElementById(actualGroup).classList.add('d-none');
+    document.getElementById(actualStep).classList.add('d-none');
+    document.getElementById(nextGroup).classList.remove('d-none');
+    document.getElementById(nextStep).classList.remove('d-none'); 
+    if (group ==1) {
+
+
+    }
+}
+function prevForm(group, step) {
+    console.log(group);
+    let actualGroup = "group" + group;  // Obtener el ID del grupo actual
+    let actualStep = actualGroup + "-" + step;
+    let prevGroup = actualGroup;
+    let prevStep = actualGroup + "-" + (step - 1);
+
+    // Lógica para retroceder al grupo y paso anteriores según los casos
+    if (group == 1 && step == 1) {
+        // Si estamos en el grupo 1, paso 1, no hay grupo anterior
+        prevGroup = null;
+        prevStep = null;
+    } else if (group == 2 && step == 1) {
+        prevGroup = "group" + (group - 1); 
+        prevStep = prevGroup + "-1"; // Retrocedemos a 1-1 del grupo 1
+    } else if (group == 2 && step > 1) {
+        prevGroup = "group" + group;
+        prevStep = prevGroup + "-" + (step - 1); // Retrocedemos dentro del grupo 2
+    } else if (group == 3 && step == 1) {
+        // Si estamos en 3-1, retrocedemos a 2-4
+        prevGroup = "group" + (group - 1);
+        prevStep = prevGroup + "-4"; // Retrocedemos al paso 4 del grupo 2
+    } else if (group == 3 && step > 1) {
+        prevGroup = "group" + group;
+        prevStep = prevGroup + "-" + (step - 1); // Retrocedemos dentro del grupo 3 (3-3 -> 3-2 -> 3-1)
+    } else if (group == 4 && step == 1) {
+        // Si estamos en el grupo 4, paso 1, retrocedemos a 3-3
+        prevGroup = "group" + (group - 1); 
+        prevStep = prevGroup + "-3"; // Retrocedemos al paso 3 del grupo 3
+    }
+
+    console.log(actualGroup);
+    console.log(actualStep);
+    console.log("--------");
+    console.log(prevGroup);
+    console.log(prevStep);
+
+    if (prevGroup !== null && prevStep !== null) {
+        document.getElementById(actualGroup).classList.add('d-none');
+        document.getElementById(actualStep).classList.add('d-none');
+        document.getElementById(prevGroup).classList.remove('d-none');
+        document.getElementById(prevStep).classList.remove('d-none');
+    }
+}
---
+++ src/js/index.js
@@ -0,0 +1,93 @@
+function calcularROSA() {
+    let puntos = 0;
+    const form = document.getElementById('rosaForm')
+    // Tiempo de uso diario
+    const usoDiario = document.querySelector('input[name="tiempoUso"]:checked').value;
+    if (usoDiario === 'alto') puntos += 2;
+
+    // Altura del asiento
+    const alturaSilla = document.querySelector('input[name="alturaSilla"]:checked').value;
+    if (alturaSilla !== 'neutra') puntos += 1;
+
+    // Criterios adicionales para altura de la silla
+    if (document.querySelector('input[name="alturaSillaAdicional"][value="espacio"]').checked) puntos += 1;
+    if (document.querySelector('input[name="alturaSillaAdicional"][value="noAjustable"]').checked) puntos += 1;
+
+    // Profundidad del asiento
+    const profundidadSilla = document.querySelector('input[name="profundidadSilla"]:checked').value;
+    if (profundidadSilla !== 'neutra') puntos += 1;
+
+    // Criterios adicionales para profundidad de la silla
+    if (document.querySelector('input[name="profundidadSillaAdicional"][value="noAjustable"]').checked) puntos += 1;
+
+    // Reposabrazos
+    const reposabrazos = document.querySelector('input[name="reposabrazos"]:checked').value;
+    if (reposabrazos !== 'neutra') puntos += 1;
+
+    // Criterios adicionales para reposabrazos
+    if (document.querySelector('input[name="reposabrazosAdicional"][value="bordes"]').checked) puntos += 1;
+    if (document.querySelector('input[name="reposabrazosAdicional"][value="ancho"]').checked) puntos += 1;
+    if (document.querySelector('input[name="reposabrazosAdicional"][value="noAjustable"]').checked) puntos += 1;
+
+    // Respaldo
+    const respaldo = document.querySelector('input[name="respaldo"]:checked').value;
+    if (respaldo !== 'neutra') puntos += 1;
+
+    // Criterios adicionales para respaldo
+    if (document.querySelector('input[name="respaldoAdicional"][value="alto"]').checked) puntos += 1;
+    if (document.querySelector('input[name="respaldoAdicional"][value="noAjustable"]').checked) puntos += 1;
+
+    // Teléfono
+    const telefono = document.querySelector('input[name="telefono"]:checked').value;
+    if (telefono !== 'neutra') puntos += 1;
+
+    // Criterios adicionales para teléfono
+    if (document.querySelector('input[name="telefonoAdicional"][value="hombro"]').checked) puntos += 1;
+    if (document.querySelector('input[name="telefonoAdicional"][value="sinManosLibres"]').checked) puntos += 1;
+
+    // Altura de los ojos
+    const alturaOjos = document.querySelector('input[name="alturaOjos"]:checked').value;
+    if (alturaOjos !== 'neutra') puntos += 1;
+
+    // Criterios adicionales para altura de los ojos
+    if (document.querySelector('input[name="alturaOjosAdicional"][value="distancia"]').checked) puntos += 1;
+    if (document.querySelector('input[name="alturaOjosAdicional"][value="giro"]').checked) puntos += 1;
+    if (document.querySelector('input[name="alturaOjosAdicional"][value="portadocumentos"]').checked) puntos += 1;
+    if (document.querySelector('input[name="alturaOjosAdicional"][value="reflejos"]').checked) puntos += 1;
+
+    // Ratón
+    const raton = document.querySelector('input[name="raton"]:checked').value;
+    if (raton !== 'neutra') puntos += 1;
+
+    // Criterios adicionales para ratón
+    if (document.querySelector('input[name="ratonAdicional"][value="pequeno"]').checked) puntos += 1;
+    if (document.querySelector('input[name="ratonAdicional"][value="diferenteAltura"]').checked) puntos += 1;
+    if (document.querySelector('input[name="ratonAdicional"][value="reposamanos"]').checked) puntos += 1;
+
+    // Teclado
+    const teclado = document.querySelector('input[name="teclado"]:checked').value;
+    if (teclado !== 'neutra') puntos += 1;
+
+    // Criterios adicionales para teclado
+    if (document.querySelector('input[name="tecladoAdicional"][value="desviacionEscribir"]').checked) puntos += 1;
+    if (document.querySelector('input[name="tecladoAdicional"][value="elevado"]').checked) puntos += 1;
+    if (document.querySelector('input[name="tecladoAdicional"][value="soporteNoAjustable"]').checked) puntos += 1;
+
+    // Resultados
+    const resultadoDiv = document.getElementById('result');
+    resultadoDiv.innerHTML = `<h2>Resultado de la Evaluación</h2><h4>Puntuación total: ${puntos}</h4>`;
+    form.classList.add('d-none')
+    if (puntos < 5) {
+        resultadoDiv.innerHTML += "<h4>La ergonomía de su espacio de trabajo es adecuada.</h4>";
+    } else if (puntos < 10) {
+        resultadoDiv.innerHTML += "<h4>Hay algunas áreas que necesitan mejoras ergonómicas.</h4>";
+    } else {
+        resultadoDiv.innerHTML += "<h4>Se requieren cambios significativos para mejorar la ergonomía.</h4>";
+    }
+
+    document.getElementById('rosaForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        calcularROSA();
+    });
+    
+}
